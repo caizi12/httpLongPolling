80c458bb983f3ac9a1b25c261bfd96293ce0a085
liliang.lll@alibaba-inc.com <liliang.lll@alibaba-inc.com>
2018-01-17 19:20:38 +0800

    add file

--- algorithm/src/test/java/com/lf/algorithm/ratio/ProductRatio.java
+++ algorithm/src/test/java/com/lf/algorithm/ratio/ProductRatio.java
@@ -8,31 +8,32 @@ import java.util.*;
  */
 public class ProductRatio {
 
-    private static int MAX_DESCARTES = 100000;
-
     public static void main(String[] args) {
 
         Map<Integer, Integer> paramData = new HashMap<Integer, Integer>();
-        for (int i = 0; i < 4; i++) {
-            paramData.put(i, new Random().nextInt(100) + 20);
+        for (int i = 0; i < 3; i++) {
+            paramData.put(i, new Random().nextInt(100) +  new Random().nextInt(100));
         }
 
-        System.out.println(calcuate(1000000, paramData));
+        System.out.println(calcuate(950000, paramData));
     }
 
     public static long calcuate(int topNum, Map<Integer, Integer> paramData) {
 
 
-        List resultEntityPopList = new ArrayList();
         long descartesNum = 1;
         for (Map.Entry<Integer, Integer> entry : paramData.entrySet()) {
             descartesNum *= entry.getValue();
         }
 
+        if(descartesNum<=topNum){
+            return descartesNum;
+        }
+
         //计算待要减少的倍数
-        double reduceMultiple = descartesNum / topNum;
+        double reduceMultiple =descartesNum / topNum;
         //计算倍数因子，计算倍数的N次方根
-        double multipleDivisor = StrictMath.pow(reduceMultiple, 1.0 / paramData.size());
+         double multipleDivisor = StrictMath.pow(reduceMultiple, 1.0 / paramData.size());
         double tagTypeMaxSize = 0;
         BigDecimal num1 = null;
         BigDecimal num2 = null;
@@ -42,7 +43,7 @@ public class ProductRatio {
             num1 = new BigDecimal(entry.getValue());
             num2 = new BigDecimal(multipleDivisor);
 
-            tagTypeMaxSize = num1.divide(num2, BigDecimal.ROUND_DOWN).doubleValue();
+            tagTypeMaxSize = num1.divide(num2, BigDecimal.ROUND_DOWN).intValue();
             newDescartesNum *= tagTypeMaxSize;
         }
 
